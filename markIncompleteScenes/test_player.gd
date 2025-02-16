dd47c41c9ffc0e58bc724caa3575c20e7b0c898a
msernoskie <mark.sernoskie@gmail.com>
2025-02-15 20:50:28 -0500

    Basic Frameworks
    Town Script containing its many integers
    Character and NPC integration nearing completion.
    Node Created for Interactable.
    Attach interactable to any node that is to be interacted with by the player.
    
    Basic time of day system scripted in Town. Call the function to advance time, and it will emit a signal for what time of day it is

---
+++ markIncompleteScenes/test_player.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+@onready var animation: AnimatedSprite2D = $AnimatedSprite2D
+@onready var chat_box: Label = $Label
+@onready var chat_timer: Timer = $Timer
+
+signal isWalking
+var isTalking: bool = false
+var show_npc_text
+func _ready():
+	animation.play_idle()
+	
+func _physics_process(delta: float) -> void:
+	if !isTalking:
+		if Input.is_action_just_pressed("interact"):
+			debug_npc_chat()
+		var direction = Input.get_vector("move_left","move_right","move_up","move_down")
+		velocity = direction * 500
+		move_and_slide()
+	
+	if velocity.length() > 0.0:
+		animation.play_walk()
+		isWalking.emit()
+	else:  
+		animation.play_idle()
+		
+func _on_is_walking() -> void:
+	if velocity.x < 0:
+		animation.flip_h = true
+	elif velocity.x > 0:
+		animation.flip_h = false
+
+func debug_npc_chat():
+	chat_box.set_visible(true)
+	chat_timer.start()
+	
+func _on_interactable_on_interact(text) -> void:
+		chat_box.set_text(text)
+
+func _on_timer_timeout() -> void:
+	chat_box.set_visible(false)
