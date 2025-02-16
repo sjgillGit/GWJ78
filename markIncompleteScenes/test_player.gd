cd15f881b09a229da9b81d2727b64a989ab312d4
msernoskie <mark.sernoskie@gmail.com>
2025-02-16 13:39:30 -0500

    Exports
    Added Exports to Town, Player, NPC
    - Changing the alignment enum in Town will have it check all children for nodes that can have the alignment updated, and will trigger the changes.

--- markIncompleteScenes/test_player.gd
+++ markIncompleteScenes/test_player.gd
@@ -2,10 +2,12 @@ extends CharacterBody2D
 @onready var animation: AnimatedSprite2D = $AnimatedSprite2D
 @onready var chat_box: Label = $Label
 @onready var chat_timer: Timer = $Timer
-
 signal isWalking
 var isTalking: bool = false
 var show_npc_text
+@export_category("Player Properties")
+@export var movement_speed: int = 500
+
 func _ready():
 	animation.play_idle()
 	
@@ -14,7 +16,7 @@ func _physics_process(delta: float) -> void:
 		if Input.is_action_just_pressed("interact"):
 			debug_npc_chat()
 		var direction = Input.get_vector("move_left","move_right","move_up","move_down")
-		velocity = direction * 500
+		velocity = direction * movement_speed
 		move_and_slide()
 	
 	if velocity.length() > 0.0:
--- markIncompleteScenes/test_town.gd
+++ markIncompleteScenes/test_town.gd
@@ -1,16 +1,20 @@
 extends Node2D
 signal timeOfDayCycle
-var time_of_day = "morning"
+@export_category("Time of Day")
+@export_enum("MORNING", "NOON", "NIGHT") var time_of_day = "MORNING"
 #Simple to implement
-var water_quality: int = 100
-var villager_happiness: int = 100
-var population_health: int = 100
-var chaos: int = 0
-var good: int = 0
-#Complex
-var admin: int = 100
-var emergency_services: int = 100
-var gdp: int = 100 #May need an array or Vector2D
+@export_category("City Health")
+@export_range(0,100,0.1) var water_quality : float
+@export_range(0,100,0.1)var villager_happiness: float
+@export_range(0,100,0.1) var population_health: float
+@export_subgroup("Player alignment")
+@export_range(0,100,0.1) var chaos: float
+@export_range(0,100,0.1) var good: float
+@export_enum("GOOD", "CHAOS", "NEUTRAL") var player_alignment = "NEUTRAL"
+@export_subgroup("Complex Undefined City Health Systems")
+@export_range(0,100,0.1) var admin: float
+@export_range(0,100,0.1) var emergency_services: float
+@export_range(0,100,0.1) var gdp: float #May need an array or Vector2D
 
 var game_seed = 1
 
@@ -21,6 +25,22 @@ var eventPool:Array = []
 var decisionLog:Array = []
 
 
+
+#_physics_process will not likely be used in its current state for the update system, this is a place holder for testing
+func _physics_process(delta: float) -> void:
+	var current_alignment = player_alignment #Store the current alignment
+	_alignment_threshold_check()			 #Check if the alignment values meet the criteria for an update
+	if current_alignment != player_alignment:#if the alignment has changed the method will update children nodes on the players new alignment
+		_update_alignment()
+	
+func _alignment_threshold_check():
+	if chaos >= 50 and good < 50:
+		player_alignment = "CHAOS"
+	elif good >= 50 and chaos < 50:
+		player_alignment = "GOOD"
+	else: pass
+		
+	
 #Start new game
 #Call required functions to wipe and set up the game
 #Pull  events from the pool based on modulo division and add x amount to event pool
@@ -46,60 +66,69 @@ func set_time_of_day(time):
 	time_of_day = time
 	
 func advance_time():
-	if time_of_day == "morning":
-		set_time_of_day("noon")
-		emit_signal("time_of_day", "noon")
-	elif time_of_day == "noon":
-		set_time_of_day("night")
-		emit_signal("time_of_day", "night")
-	elif time_of_day == "night":
-		set_time_of_day("morning")
-		emit_signal("time_of_day", "morning")
+	if time_of_day == "MORNING":
+		set_time_of_day("NOON")
+		emit_signal("time_of_day", "NOON")
+	elif time_of_day == "NOON":
+		set_time_of_day("NIGHT")
+		emit_signal("time_of_day", "NIGHT")
+	elif time_of_day == "NIGHT":
+		set_time_of_day("MORNING")
+		emit_signal("time_of_day", "MORNING")
 	else: 
 		print("advance_time error, setting to morning")
-		set_time_of_day("morning")
+		set_time_of_day("MORNING")
 		
 		
+
+#Signal Emitting to children for threshholds hit
+func _update_alignment():
+	for child in self.get_children():
+		if child.has_method("alignment_update"):
+			child.alignment_update(player_alignment)
+
+
+
 func _get_water_quality():
-	pass
+	return water_quality 
 func _set_water_quality(water):
 	pass
 func _get_villager_happiness():
-	pass
+	return villager_happiness
 func _set_villager_happiness(happy):
 	pass
 func _get_population_health():
-	pass
+	return population_health
 func _set_population_health(health):
 	pass
 func _get_chaos():
-	pass
+	return chaos
 func _set_chaos(chaos):
 	pass
 func _get_good():
-	pass
+	return good
 func _set_good(good):
 	pass
 func _get_admin():
-	pass
+	return admin
 func _set_admin(admin):
 	pass
 func _get_emergency_services():
-	pass
+	return emergency_services
 func _set_emergency_services(emerg):
 	pass
 #May need to be a more complex data type, research required before creating.
 func _get_gpd():
-	pass
+	return gdp
 func _set_gdp(gdp):
 	pass
-	
+
 func _get_eventPool():
-	pass
+	return eventPool
 func _set_eventPool(event):
 	pass
 func _get_decisionLog():
-	pass
+	return decisionLog
 func _set_decisionLog(log):
 	pass	
 #Create Array of NPC's. Instanciate x times and add to array
