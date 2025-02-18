2f890fcdfea03ccb4e2c24a1db59480abec5467d
Tomek <tomasz.glowacki.work@gmail.com>
2025-02-18 23:49:19 +0100

    Air dash and Air drop added. Enchanced jump functionality.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,15 +3,19 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 const MAX_SPEED = 200.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -350.0
 const ACCELERATION = 5.0
 const SPEED_DELTA = 20.0
+
+@export var direction = 0
 
 @onready var label: Label = $Label
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 var dayS:bool = true
 var isMoving:bool = true
+var isDashing:bool = false
+var timeInAir:float = 0
 
 var idleAnim:String = "idle_day"
 var runAnim:String = "run_day"
@@ -19,13 +23,18 @@ var jumpAnim:String = "jump_day"
 
 func _physics_process(delta: float) -> void:
 	#table for 
-	label.text = "Velocity = %d" % velocity.x
+	label.text = "Velocity.x = %d \nVelocity.y = %d" % [velocity.x, velocity.y]
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
+		if not isDashing:
+			velocity += get_gravity() * delta * 1.2
+		timeInAir += delta
+	else:
+		timeInAir = 0
+	
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or timeInAir < 0.1):
 		velocity.y = JUMP_VELOCITY
 		
 	if Input.is_action_just_pressed("interact"):
@@ -39,7 +48,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
+	direction = Input.get_axis("move_left", "move_right")
 	
 	if direction > 0:
 		animated_sprite_2d.flip_h = false
@@ -57,16 +66,16 @@ func _physics_process(delta: float) -> void:
 	
 	
 		
-	if direction:
-		if Input.is_action_pressed("sprint"):
+	if direction and isMoving:
+		if Input.is_action_pressed("sprint") and is_on_floor():
 			if abs(velocity.x) <= MAX_SPEED:
 				velocity.x += ACCELERATION * direction
 		else:
 			velocity.x = move_toward(velocity.x, direction * SPEED, SPEED_DELTA)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED_DELTA)
-	if isMoving:
-		move_and_slide()
+		
+	move_and_slide()
 		
 
 	
