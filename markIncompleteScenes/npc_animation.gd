b46a016f1074628b0e64b84f64ffb913bef374e8
Tomek <tomasz.glowacki.work@gmail.com>
2025-02-20 23:09:23 +0100

    Swinging platform added

--- markIncompleteScenes/npc_animation.gd
+++
@@ -1,9 +0,0 @@
-extends AnimatedSprite2D
-func play_idle():
-	play("idle")
-	
-#func play_walk():
-#	play("walk")
-	
-#func play_death():
-#	play("death")
--- markIncompleteScenes/scripts/townModifier.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-#This will be used on all sources that alter properties in town
-#attach to entities which will alter, those events call to trigger a method here which,
-#will emit the event, and the variable
