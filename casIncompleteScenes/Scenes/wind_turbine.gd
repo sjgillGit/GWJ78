499d9f353a487f5f1ff6ca4c77bc97d1ee893f22
Tomek <tomasz.glowacki.work@gmail.com>
2025-02-20 03:22:27 +0100

    wind gust finish

--- casIncompleteScenes/Scenes/wind_turbine.gd
+++ casIncompleteScenes/Scenes/wind_turbine.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
-
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 @export var windStrength:float = 50
+@export var facingLeft:bool = true
 
 var bodyInArea: CharacterBody2D = null
 var areaStart: float
@@ -16,16 +16,24 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	if bodyInArea:
-		var posDiff = (bodyInArea.global_position.x + bodyInArea.get_node("CollisionShape2D").shape.radius) - areaStart
-		if posDiff > 0:
-			bodyInArea.velocity.x -= windStrength
+		var posDiff
+		if scale.x == 1:
+			posDiff = (bodyInArea.global_position.x + bodyInArea.get_node("CollisionShape2D").shape.radius) - areaStart
+			if posDiff > 0:
+				bodyInArea.velocity.x -= windStrength
+		elif scale.x == -1:
+			posDiff = (bodyInArea.global_position.x - bodyInArea.get_node("CollisionShape2D").shape.radius) - areaStart
+			if posDiff < 0:
+				bodyInArea.velocity.x += windStrength
 			
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
-
-		areaStart = collision_shape_2d.global_position.x - collision_shape_2d.shape.size.x/2
+		if scale.x == 1:
+			areaStart = collision_shape_2d.global_position.x - collision_shape_2d.shape.size.x/2
+		elif scale.x == -1:
+			areaStart = collision_shape_2d.global_position.x + collision_shape_2d.shape.size.x/2
 		bodyInArea = body
 
 
