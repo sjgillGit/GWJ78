f306d373884f6b8d95de5dea658b42e0dbfac8c5
msernoskie <mark.sernoskie@gmail.com>
2025-02-22 18:02:51 -0500

    Updated world script for all nodes

--- Scripts/world_manager.gd
+++ Scripts/world_manager.gd
@@ -3,15 +3,19 @@ extends Node2D
 @onready var people_layer: TileMapLayer = $people_toggle
 @onready var prosperity_layer: TileMapLayer = $prosperity_toggle
 #onready var mugger_spawn: Path2D = $"mugger spawn"
+@onready var prosperity: Node2D = $prosperity
+@onready var water: Node2D = $water
+
+
 
 func _ready() -> void:
-	if PlayerProperties.water_toggle == true:
+	if PlayerProperties.prosperity == true:
 		print("water toggle triggered on readyin ", self)
 		water_layer.queue_free()
 	if PlayerProperties.people_toggle == true:
 		print("people_toggle triggered on ready in ", self)
 		people_layer.queue_free()
-	if PlayerProperties.prosperity_toggle == true:
+	if PlayerProperties.prosperity == true:
 		prosperity_layer.queue_free()
 		print("prosperity_toggle triggered on ready in ", self)
 	
